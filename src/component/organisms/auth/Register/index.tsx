b02a7d1c34a942bebb7644b8fc7bceec9dbd56e9
Chaos0103 <lyt1228@naver.com>
2023-12-17 19:03:04 +0900

    feat: Register 구현

---
+++ src/component/organisms/auth/Register/index.tsx
@@ -0,0 +1,12 @@
+import {RegisterContainer} from "./style";
+import RegisterForm from "../RegisterForm";
+
+function Register() {
+  return (
+    <RegisterContainer>
+      <RegisterForm/>
+    </RegisterContainer>
+  );
+}
+
+export default Register;
---
+++ src/component/organisms/auth/Register/style.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const RegisterContainer = styled.div`
+    position: relative;
+    height: calc(100vh - 4rem);
+
+    margin: auto;
+
+    width: 30%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+`;
