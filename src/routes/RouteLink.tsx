05307abe03439b848ea7c6149606ef4d75dfd681
Chaos0103 <lyt1228@naver.com>
2023-12-18 00:30:53 +0900

    feat: 공지사항 페이지 라우터 적용

--- src/routes/RouteLink.tsx
+++ src/routes/RouteLink.tsx
@@ -4,21 +4,25 @@ import PrivateRouter from "./PrivateRouter";
 import AttractionPage from "../pages/attraction/AttractionPage";
 import LoginPage from "../pages/auth/LoginPage";
 import RegisterPage from "../pages/auth/RegisterPage";
+import NoticePage from "../pages/notice/NoticePage";
+import NoticeDetailPage from "../pages/notice/NoticeDetailPage";
 
 const RouteLink = () => {
   return (
     <>
       <Routes>
         <Route Component={PublicRouter}>
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/signup' element={<RegisterPage />} />
-          <Route path='/attractions' element={<AttractionPage />} />
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='/signup' element={<RegisterPage/>}/>
+          <Route path='/attractions' element={<AttractionPage/>}/>
         </Route>
-        <Route Component={PrivateRouter}>
-          <Route path='/posts' element={null} />
-          <Route path='/hot-places' element={null} />
-          <Route path='/plans' element={null} />
-        </Route>
+        {/*<Route Component={PrivateRouter}>*/}
+        <Route path='/posts' element={null}/>
+        <Route path='/hot-places' element={null}/>
+        <Route path='/plans' element={null}/>
+        <Route path='/notices' element={<NoticePage/>}/>
+        <Route path='/notices/:noticeId' element={<NoticeDetailPage/>}/>
+        {/*</Route>*/}
       </Routes>
     </>
   );
