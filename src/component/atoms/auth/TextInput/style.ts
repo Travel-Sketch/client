d14d69f08c4a3ee2cab24ed34099a1beb8e6397d
Chaos0103 <lyt1228@naver.com>
2023-12-17 16:16:07 +0900

    feat: TextInputWrapper 구현

---
+++ src/component/atoms/auth/TextInput/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const TextInputWrapper = styled.div`
+    width: 100%;
+    
+    div {
+        width: 100%;
+        height: 48px;
+        
+        label {
+            //font-family: 'Pretendard';
+            font-size: 0.9rem;
+            line-height: 1rem;
+        }
+    }
+`
