f4882050334ea5efce008c45fb94f197b6987000
Chaos0103 <lyt1228@naver.com>
2023-12-18 00:30:31 +0900

    feat: 공지사항 상세 페이지 구현

---
+++ src/pages/notice/NoticeDetailPage.tsx
@@ -0,0 +1,9 @@
+import NoticeDetailPageLayout from "../../layouts/page/notice/NoticeDetailPageLayout";
+
+function NoticeDetailPage() {
+  return (
+    <NoticeDetailPageLayout/>
+  );
+}
+
+export default NoticeDetailPage;
