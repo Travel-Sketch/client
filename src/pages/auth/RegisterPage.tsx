a02c9c5e8698aa040ce1ee23c49e13de37a2fa60
Chaos0103 <lyt1228@naver.com>
2023-12-17 19:03:12 +0900

    feat: RegisterPage 구현

---
+++ src/pages/auth/RegisterPage.tsx
@@ -0,0 +1,9 @@
+import RegisterPageLayout from "../../layouts/page/auth/RegisterPageLayout";
+
+function RegisterPage() {
+  return (
+    <RegisterPageLayout />
+  )
+}
+
+export default RegisterPage;
