60cac7fad326479d6982ac33071ec8d017ae7cec
Chaos0103 <lyt1228@naver.com>
2023-12-17 19:02:53 +0900

    feat: RegisterPageLayout 구현

---
+++ src/layouts/page/auth/RegisterPageLayout/index.tsx
@@ -0,0 +1,12 @@
+import {RegisterPageLayoutContainer} from "./style";
+import Register from "../../../../component/organisms/auth/Register";
+
+function RegisterPageLayout() {
+  return (
+    <RegisterPageLayoutContainer>
+      <Register />
+    </RegisterPageLayoutContainer>
+  )
+}
+
+export default RegisterPageLayout;
