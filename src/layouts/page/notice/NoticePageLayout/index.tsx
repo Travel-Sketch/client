be166e3cdb3861b0883fc4b96004b0d0d03051a4
Chaos0103 <lyt1228@naver.com>
2023-12-18 00:30:01 +0900

    feat: 공지사항 페이지 레이아웃 구현

---
+++ src/layouts/page/notice/NoticePageLayout/index.tsx
@@ -0,0 +1,12 @@
+import {NoticePageLayoutContainer} from "./style";
+import NoticeList from "../../../../component/organisms/notice/NoticeList";
+
+function NoticePageLayout() {
+  return (
+    <NoticePageLayoutContainer>
+      <NoticeList />
+    </NoticePageLayoutContainer>
+  )
+}
+
+export default NoticePageLayout;
---
+++ src/layouts/page/notice/NoticePageLayout/style.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const NoticePageLayoutContainer = styled.div`
+    position: relative;
+    margin-top: 3rem;
+`;
